f71abc11002e75a3ba6b4ea566e760da5769a4a8
geneva-lake <agljamov_blum@aol.com>
2017-04-27 17:54:47 +0300

    formtatting

--- assets/store.js
+++ assets/store.js
@@ -7,27 +7,22 @@ import DataManager from './data-manager';
 var CHANGE_EVENT = 'change';
 
 class DataStoreClass extends EventEmitter {
-
-	constructor() {
-		super();
-		this.currentText = "one";
-	}
-
-	emitChange() {
-		this.emit(CHANGE_EVENT);
-	}
-
-	addChangeListener(callback) {
-		this.on(CHANGE_EVENT, callback);
-	}
-
-	getCurrentText() {
-		return this.currentText;
-	}
-	setCurrentText(text) {
-		this.currentText =text;
-	}
-
+  constructor() {
+    super();
+    this.currentText = "one";
+  }
+  emitChange() {
+    this.emit(CHANGE_EVENT);
+  }
+  addChangeListener(callback) {
+    this.on(CHANGE_EVENT, callback);
+  }
+  getCurrentText() {
+    return this.currentText;
+  }
+  setCurrentText(text) {
+    this.currentText =text;
+  }
   getTextFromServer(number) {
     DataManager.get(number);
   }
@@ -36,19 +31,19 @@ class DataStoreClass extends EventEmitter {
 var DataStore = new DataStoreClass();
 
 AppDispatcher.register(function (action) {
-	switch (action.type) {
-	case "RECEIVE_TEXT":
-		console.log('Text received');
+  switch (action.type) {
+  case "RECEIVE_TEXT":
+    console.log('Text received');
     DataStore.setCurrentText(action.text);
-		DataStore.emitChange();
-		break;
+    DataStore.emitChange();
+  break;
   case "GET_TEXT":
-      console.log('The button was clicked store.');
-      var number = action.number;
-      DataStore.getTextFromServer(number);
-      break;
-	default:
-	}
+    console.log('The button was clicked store.');
+    var number = action.number;
+    DataStore.getTextFromServer(number);
+  break;
+  default:
+  }
 });
 
 export default DataStore;
