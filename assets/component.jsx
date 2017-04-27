3b2e27854506e4585b9ce9e05cd5e829f45e3b4e
geneva-lake <agljamov_blum@aol.com>
2017-04-27 17:59:36 +0300

    Formatting

--- assets/component.jsx
+++ assets/component.jsx
@@ -2,48 +2,44 @@ import React from 'react';
 import ButtonActions from './action.js';
 import DataStore from './store.js';
 
-
-
 class App extends React.Component {
+	
+  constructor(props) {
+    super(props);
+    this.onChange = this.onChange.bind(this)
+    this.state = {
+      text : DataStore.getCurrentText(),
+      number : -1
+    };
+    this.onClick = this.onClick.bind(this)
+  }
+  componentDidMount() {
+    DataStore.addChangeListener(this.onChange);
+  }
 
-	constructor(props) {
-		super(props);
-		this.onChange = this.onChange.bind(this)
-		this.state = {
-			text : DataStore.getCurrentText(),
-			number : -1
-		};
-		this.onClick = this.onClick.bind(this)
-	}
+  render() {
+    return (
+      <div>
+        <button onClick = {this.onClick}>button</button>
+        <div>{this.state.text}</div>
+      </div> );
+  }
 
-	componentDidMount() {
-		DataStore.addChangeListener(this.onChange);
-	}
+  onClick() {
+    console.log('The button was clicked.');
+    this.setState({
+      number : this.state.number * -1
+    });
+    ButtonActions.getTextFromServer(this.state.number);
+  }
 
-	render() {
-		return (
-			 <div>
-				 <button onClick = {this.onClick}>button</button>
-			   <div>{this.state.text}</div>
-			 </div> );
-	}
-
-	onClick() {
-		console.log('The button was clicked.');
-		this.setState({
-			number : this.state.number * -1
-		});
-		ButtonActions.getTextFromServer(this.state.number);
-	}
-
-	onChange() {
-		console.log('The button was clicked component.');
-		this.setState({
-			text : DataStore.getCurrentText()
-		});
-		console.log(this.state.text);
-	}
-
+  onChange() {
+    console.log('The button was clicked component.');
+    this.setState({
+      text : DataStore.getCurrentText()
+    });
+    console.log(this.state.text);
+  }
 }
 
 export default App;
